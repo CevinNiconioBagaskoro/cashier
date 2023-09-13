b62077b2376fe8a7af3cc284c43ae79e22a7d37c
Cevin <niconiocevin@gmail.com>
2023-09-13 14:34:27 +0700

    Create Barang Service

---
+++ src/test/java/com/smk/cashier/service/BarangServiceTest.java
@@ -0,0 +1,34 @@
+package com.smk.cashier.service;
+
+import com.smk.cashier.model.Barang;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BarangServiceTest {
+
+    @Test
+    void getBarangList() {
+        List<Barang> barangList = BarangService.getInstance().getBarangList();
+        assertEquals(barangList.size(),2);
+    }
+
+    @Test
+    @Order(1)
+    void addBarang() {
+        Barang laptop = new Barang();
+        laptop.setKodeBarang("LP001");
+        laptop.setNamaBarang("Laptop");
+        laptop.setHargaBarang(5000000);
+        BarangService.getInstance()
+                .addBarang(laptop);
+        Barang mouse = new Barang();
+        mouse.setKodeBarang("MO001");
+        mouse.setNamaBarang("Mouse");
+        mouse.setHargaBarang(100000);
+        BarangService.getInstance().addBarang(mouse);
+    }
+}
